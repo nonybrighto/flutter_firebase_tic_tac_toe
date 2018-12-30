d0e0a921b5d9cd4d9698196fe10dfbb0623667e3
nonybright <nonybrighto@gmail.com>
2018-12-30 20:33:22 +0100

    customize app theme

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,10 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Color(0XFF212845),
+        scaffoldBackgroundColor: Color(0XFF212845),
+        brightness: Brightness.dark,
+        primarySwatch: Colors.yellow,
       ),
       home:MenuPage(),
     );
