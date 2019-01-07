e2b684bbadb79b3eb5af34599f4ddfe0e20a19c0
nonybright <nonybrighto@gmail.com>
2019-01-07 06:35:57 +0100

    Implement copyWith

--- lib/models/User.dart
+++ lib/models/User.dart
@@ -1,13 +1,37 @@
-enum  UserState{
-  available, playing , away, offline
-}
+enum UserState { available, playing, away, offline }
 
-class User{
+class User {
   final String id;
   final String name;
   final String email;
   final String avatarUrl;
   final UserState currentState;
+  final String fcmToken;
 
-  User({this.id, this.name, this.email, this.avatarUrl, this.currentState});
+  User(
+      {this.id,
+      this.name,
+      this.email,
+      this.avatarUrl,
+      this.currentState,
+      this.fcmToken});
+
+  User copyWith({
+    String id,
+    String name,
+    String email,
+    String avatarUrl,
+    UserState currentState,
+    String fcmToken,
+  }) {
+
+    return User(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      email: email ?? this.email,
+      avatarUrl: avatarUrl ?? this.avatarUrl,
+      currentState: currentState ?? this.currentState,
+      fcmToken: fcmToken ?? this.fcmToken,
+    );
+  }
 }
