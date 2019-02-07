72ffaa8a2ba4b8ec8ecaedb773655732b1c011e8
nonybright <nonybrighto@gmail.com>
2019-02-07 20:06:38 +0100

    update handlechallenge accept to use async

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -5,7 +5,7 @@ var app = admin.initializeApp();
 
 //NB: No major security feature implemented such as CSRF protection
 
-exports.handleChallenge = functions.https.onRequest((request, response) => {
+exports.handleChallenge = functions.https.onRequest(async (request, response) => {
 
     let senderId = request.query.senderId;
     let senderName = request.query.senderName;
@@ -35,79 +35,54 @@ exports.handleChallenge = functions.https.onRequest((request, response) => {
         // Send a message to the device corresponding to the provided
         // registration token.
         admin.messaging().send(message)
-            .then((res) => {
-                // Response is a message ID string.
-                console.log('Successfully sent message:', res);
-                return response.send(true);
-            })
-            .catch((error) => {
-                console.log('Error sending message:', error);
-                response.send(false);
-            });
+        .then((res) => {
+            // Response is a message ID string.
+            console.log('Successfully sent message:', res);
+            return response.send(true);
+        })
+        .catch((error) => {
+            console.log('Error sending message:', error);
+            response.send(false);
+        });
 
     } else if (handleType === 'accept') {
 
-        //TODO: create the game of the two users and send notification to the two users
-        //games_collection .. Id(combined_user_id) .. data  
         let gameId = receiverId + '_' + senderId;
-        admin.firestore().collection('games').doc(gameId).set({
-            //The player who sent the challenge is always player1 and will always be X.
-            player1: {
-                user: {
-                    id: receiverId,
-                    name: receiverName,
-                    fcmToken: receiverFcmToken
-                },
-                gamePiece: 'X',
-                score: 0
+        // Check if the game already exists, if the last game was won by someone, start a new one instead.
+        // It shows that the game was completeda already
+
+        let notificationMessage;
+       try{
+        let docSnapshot = admin.firestore().collection('games').doc(gameId).get();
+        if(!docSnapshot.exists || (docSnapshot.exists && docSnapshot.data().winner !== '')){
+            await _createGame(gameId, receiverId, receiverName, receiverFcmToken, senderId, senderName, senderFcmToken);
+            await admin.messaging().subscribeToTopic([senderFcmToken, receiverFcmToken], gameId);
+            notificationMessage = 'Your game has been started!!!';
+        }else{
+            notificationMessage = 'Your game is being continued!!!';
+        }
+
+        let message = {
+            data: {
+                player1Id: receiverId, // The player that sent the challenge is player1
+                player2Id: senderId,
+                player1Name: receiverName,
+                player2Name: senderName,
+                notificationType: 'started',
             },
-            player2: {
-                user: {
-                    id: senderId,
-                    name: senderName,
-                    fcmToken: senderFcmToken
-                },
-                gamePiece: 'O',
-                score: 0
-            },
-            winner: '',
-            currentPlayer: receiverId,
-            pieces: {
-                0: '',
-                1: '',
-                2: '',
-                3: '',
-                4: '',
-                5: '',
-                6: '',
-                7: '',
-                8: ''
+
+            notification: {
+                title: 'Game Started',
+                body: notificationMessage
             }
+        };
+        await admin.messaging().sendToTopic(gameId, message);
+        return response.send(true);
 
-        }).then((res) => {
-            return admin.messaging().subscribeToTopic([senderFcmToken, receiverFcmToken], gameId);
-        }).then((res) => {
-            let message = {
-                data: {
-                    player1Id: receiverId, // The player that sent the challenge is player1
-                    player2Id: senderId,
-                    player1Name: receiverName,
-                    player2Name: senderName,
-                    notificationType: 'started',
-                },
-
-                notification: {
-                    title: 'Game Started',
-                    body: 'Your game has been started!!!'
-                }
-            };
-            return admin.messaging().sendToTopic(gameId, message);
-        }).then((result) => {
-            return response.send(true);
-        }).catch((error) => {
-            console.log('Error subscribing to topic:', error);
+       }catch(err){
+            console.log('Error subscribing to topic:', err);
             response.send(false);
-        });
+       }
 
     } else if (handleType === 'reject') {
         let message = {
@@ -132,10 +107,7 @@ exports.handleChallenge = functions.https.onRequest((request, response) => {
                 response.send(false);
             });
     }
-
 });
-
-
 
 exports.playPiece = functions.https.onRequest((request, response) => {
 
@@ -324,37 +296,10 @@ exports.cancelGame = functions.https.onRequest((request, response) => {
 
 });
 
-// exports.onUserStatusChanged = functions.database
-//   .ref('/status/{userId}') // Reference to the Firebase RealTime database key
-//   .onUpdate(event => {
-//       console.log('status updated');
-//       console.log(event);
-//       const usersRef = admin.firestore().collection('/users');
-
-//       console.log(event.data);
-//       console.log(event.data.state);
-      
-//       return event.data.ref.once('value')
-//       .then(statusSnapshot => snapShot.val())
-//       .then(status => {
-
-//         console.log('This is the status stuff '+ status);
-        
-//          return usersRef
-//             .doc(event.params.userId)
-//             .set({
-//               currentState: status
-//             }, { merge: true });
-//       })
-// });
 exports.onUserStatusChanged = functions.database.ref('/status/{userId}').onUpdate(
     (change, context) => {
       // Get the data written to Realtime Database
       const eventStatus = change.after.val();
-      console.log(eventStatus);
-      console.log(eventStatus.state);
-      console.log(context.params.userId);
-
       const usersRef = admin.firestore().collection('/users');
 
       return usersRef
@@ -362,56 +307,16 @@ exports.onUserStatusChanged = functions.database.ref('/status/{userId}').onUpdat
             .set({
               currentState: eventStatus.state
             }, { merge: true });
-
-    //   // Then use other event data to create a reference to the
-    //   // corresponding Firestore document.
-    //   const userStatusFirestoreRef = firestore.doc(`status/${context.params.uid}`);
-
-    //   // It is likely that the Realtime Database change that triggered
-    //   // this event has already been overwritten by a fast change in
-    //   // online / offline status, so we'll re-read the current data
-    //   // and compare the timestamps.
-    //   return change.after.ref.once('value').then((statusSnapshot) => {
-    //     const status = statusSnapshot.val();
-    //     console.log(status, eventStatus);
-    //     // If the current timestamp for this data is newer than
-    //     // the data that triggered this event, we exit this function.
-    //     if (status.last_changed > eventStatus.last_changed) {
-    //       return null;
-    //     }
-
-    //     // Otherwise, we convert the last_changed field to a Date
-    //     eventStatus.last_changed = new Date(eventStatus.last_changed);
-
-    //     // ... and write it to Firestore.
-    //     return userStatusFirestoreRef.set(eventStatus);
-     // });
     });
 
 function _isTie(pieces) {
-
-    //     Object.keys(pieces).every(key => {
-
-    //        // return pieces[key] !== '';
-
-    //         if(pieces[key] === ''){
-    //             console.log('tie false');
-    //             return false;
-    //         }
-
-    //     });
-    //    // console.log('tie true');
-    //    // return true;
-
     for (let key of Object.keys(pieces)) {
         if (pieces[key] === '') {
             console.log('tie false');
             return false;
         }
     }
-    console.log('tie true');
     return true;
-
 }
 
 function _hasWin(pieces, playerPiece) {
@@ -440,6 +345,45 @@ function _hasWin(pieces, playerPiece) {
     console.log('win false');
     return false;
 }
+
+function _createGame(gameId, receiverId, receiverName, receiverFcmToken, senderId, senderName, senderFcmToken){
+    return admin.firestore().collection('games').doc(gameId).set({
+        //The player who sent the challenge is always player1 and will always be X.
+        player1: {
+            user: {
+                id: receiverId,
+                name: receiverName,
+                fcmToken: receiverFcmToken
+            },
+            gamePiece: 'X',
+            score: 0
+        },
+        player2: {
+            user: {
+                id: senderId,
+                name: senderName,
+                fcmToken: senderFcmToken
+            },
+            gamePiece: 'O',
+            score: 0
+        },
+        winner: '',
+        currentPlayer: receiverId,
+        pieces: {
+            0: '',
+            1: '',
+            2: '',
+            3: '',
+            4: '',
+            5: '',
+            6: '',
+            7: '',
+            8: ''
+        }
+
+    });
+}
+
 
 function _changeUserState(userId, state) {
 
