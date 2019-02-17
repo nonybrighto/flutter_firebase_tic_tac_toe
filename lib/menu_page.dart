2496d5277f71093fc3da72440e5a1a4764d9befc
nonybright <nonybrighto@gmail.com>
2019-02-17 12:57:42 +0100

    Get current user form bloc and show logout button

--- lib/menu_page.dart
+++ lib/menu_page.dart
@@ -8,6 +8,7 @@ import 'package:flutter_firebase_tic_tac_toe/bloc/user_bloc.dart';
 import 'package:flutter_firebase_tic_tac_toe/game_board.dart';
 import 'package:flutter_firebase_tic_tac_toe/game_process_page.dart';
 import 'package:flutter_firebase_tic_tac_toe/high_score_board.dart';
+import 'package:flutter_firebase_tic_tac_toe/models/User.dart';
 import 'package:flutter_firebase_tic_tac_toe/models/game.dart';
 import 'package:flutter_firebase_tic_tac_toe/users_board.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -25,13 +26,10 @@ class _MenuPageState extends State<MenuPage> {
   GameBloc  _gameBloc;
   FirebaseMessaging _messaging = new FirebaseMessaging();
 
-  String username = '';
-
   @override
     void initState() {
       // TODO: implement initState
       super.initState();
-      _delGetUserNamePref(); //TODO: to delete later
 
     _messaging.configure(onLaunch: (Map<String, dynamic> message) {
       print(message);
@@ -44,9 +42,7 @@ class _MenuPageState extends State<MenuPage> {
                 
                 _showAcceptanceDialog(message);
                 break;
-              
             case 'started':
-                  //TODO:gameBlocMessage that shows on the process page and allows button to display
                   _gameBloc.startServerGame(message['data']['player1Id'], message['data']['player2Id']);
                   break;
             case 'replayGame':
@@ -78,18 +74,123 @@ class _MenuPageState extends State<MenuPage> {
 
   }
 
-  _delGetUserNamePref() async{
+ 
 
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String senderName = prefs.getString('user_name');
 
-      setState(() {
-              username = senderName ?? 'No name';
-            });
+  @override
+  Widget build(BuildContext context) {
 
+    _userBloc = BlocProvider.of(context).userBloc;
+    _gameBloc = BlocProvider.of(context).gameBloc;
+    return Scaffold(
+      backgroundColor: Color(0XFF212845),
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Positioned(
+            left: -60.0,
+            top: -75.0,
+            child: _bigLetter('X'),
+          ),
+          Positioned(
+            right: -100.0,
+            bottom: -75.0,
+            child: _bigLetter('O'),
+          ),
+          SingleChildScrollView(
+                      child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: 40.0,
+                  ),
+                  Text(
+                    'Tic Tac Toe',
+                    style: TextStyle(
+                      fontSize: 50.0,
+                      fontWeight: FontWeight.bold,),
+                  ),
+                  SizedBox(
+                    height: 40.0,
+                  ),
+                 
+                  StreamBuilder(
+                    initialData: null,
+                    stream: _userBloc.currentUser,
+                    builder: (context, currentUserSnapshot){
+                      User currentUser = currentUserSnapshot.data;
+                      return (currentUser != null)?  Text('currentUser - '+currentUser.name) : Container();
+                    },
+                  ),
+                  SizedBox(
+                    height: 40.0,
+                  ),
+
+                  _menuButton('PLAY WITH COMPUTER', (){
+                    _gameBloc.startSingleDeviceGame(GameType.computer);
+                    Navigator.of(context).push(MaterialPageRoute(builder:(index)=> GameBoard()));
+                  }),
+                   _menuButton('PLAY WITH FRIEND', 
+                    (){
+                    _gameBloc.startSingleDeviceGame(GameType.multi_device);
+                    Navigator.of(context).push(MaterialPageRoute(builder:(index)=> GameBoard()));
+                  }
+                  ),
+                  _menuButton('PLAY WITH USERS', 
+                    (){
+                    _userBloc.getUsers();
+                    Navigator.of(context).push(MaterialPageRoute(builder:(index)=> UsersBoard()));
+                  }
+                  ),
+                  _menuButton('HIGH SCORE', (){
+                    
+                    _gameBloc.getHighScores();
+                     Navigator.of(context).push(MaterialPageRoute(builder:(index)=> HighScoreBoard()));
+                  }),
+
+                    StreamBuilder(
+                    initialData: null,
+                    stream: _userBloc.currentUser,
+                    builder: (context, currentUserSnapshot){
+                      User currentUser = currentUserSnapshot.data;
+                      if (currentUser != null){ 
+                           return FlatButton(child: Text('Logout', style:  TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.blue
+                            ),), onPressed: (){
+                              //TODO: implement logout action
+                            },);
+                      }else{
+                         return Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                    Text('Play with Others?', style: TextStyle(
+                      fontSize: 18.0
+                    ),),
+                        FlatButton(child: Text('Sign In', style:  TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.blue
+                            ),), onPressed: (){
+                            Navigator.of(context).push(MaterialPageRoute(builder:(index)=> AuthPage(false)));
+                          },)
+                    ]);
+                          
+                      }
+                    },
+                  ),
+                  
+                ],
+              ),
+          ),
+        
+         
+        ],
+      ),
+    );
   }
 
-  _showGameEndDialog(Map<String, dynamic> message) async{
+   _showGameEndDialog(Map<String, dynamic> message) async{
     Future.delayed(
       Duration.zero, (){
 
@@ -195,93 +296,7 @@ _showGameRejectedDialog(Map<String, dynamic> message) async{
 
   }
 
-
-  @override
-  Widget build(BuildContext context) {
-
-    _userBloc = BlocProvider.of(context).userBloc;
-    _gameBloc = BlocProvider.of(context).gameBloc;
-    return Scaffold(
-      backgroundColor: Color(0XFF212845),
-      body: Stack(
-        children: <Widget>[
-          Positioned(
-            left: -60.0,
-            top: -75.0,
-            child: _bigLetter('X'),
-          ),
-          Positioned(
-            right: -100.0,
-            bottom: -75.0,
-            child: _bigLetter('O'),
-          ),
-         SingleChildScrollView(
-           child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                  height: 40.0,
-                ),
-                Text(
-                  'Tic Tac Toe',
-                  style: TextStyle(
-                    fontSize: 50.0,
-                    fontWeight: FontWeight.bold,),
-                ),
-                SizedBox(
-                  height: 40.0,
-                ),
-                Text('currentUser - '+username),
-                SizedBox(
-                  height: 40.0,
-                ),
-
-                _menuButton('PLAY WITH COMPUTER', (){
-                  _gameBloc.startSingleDeviceGame(GameType.computer);
-                  Navigator.of(context).push(MaterialPageRoute(builder:(index)=> GameBoard()));
-                }),
-                 _menuButton('PLAY WITH FRIEND', 
-                  (){
-                  _gameBloc.startSingleDeviceGame(GameType.multi_device);
-                  Navigator.of(context).push(MaterialPageRoute(builder:(index)=> GameBoard()));
-                }
-                ),
-                _menuButton('PLAY WITH USERS', 
-                  (){
-                  _userBloc.getUsers();
-                  Navigator.of(context).push(MaterialPageRoute(builder:(index)=> UsersBoard()));
-                }
-                ),
-                _menuButton('HIGH SCORE', (){
-                  
-                  _gameBloc.getHighScores();
-                   Navigator.of(context).push(MaterialPageRoute(builder:(index)=> HighScoreBoard()));
-                }),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                  Text('Play with Others?', style: TextStyle(
-                    fontSize: 18.0
-                  ),),
-                  FlatButton(child: Text('Sign In', style:  TextStyle(
-                    fontSize: 18.0,
-                    color: Colors.blue
-                  ),), onPressed: (){
-                   Navigator.of(context).push(MaterialPageRoute(builder:(index)=> AuthPage(false)));
-                },)
-                ],)
-              ],
-            ),
-        
-         )
-        ],
-      ),
-    );
-  }
-}
-
-_menuButton(String text, onPressed) {
+  _menuButton(String text, onPressed) {
   return Padding(
     padding: const EdgeInsets.only(bottom: 30.0),
     child: SizedBox(
@@ -305,3 +320,8 @@ _bigLetter(String letter) {
     ),
   );
 }
+}
+
+
+
+
