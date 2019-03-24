e07f8a991afd870431d6754aa2d519981a8f87ac
nonybright <nonybrighto@gmail.com>
2019-03-24 11:24:42 +0100

    refactor: create function for get request

--- lib/services/game_service.dart
+++ lib/services/game_service.dart
@@ -1,105 +1,58 @@
 import 'dart:async';
 
+import 'package:flutter_firebase_tic_tac_toe/models/User.dart';
 import 'package:flutter_firebase_tic_tac_toe/models/game.dart';
 import 'package:http/http.dart' as http;
 
 class GameService{
 
 
-  Future<bool> handleChallenge(String senderId, String senderName, String senderFcmToken, String receiverId, String receiverName, String receiverFcmToken, ChallengeHandleType handleType) async{
+  Future<bool> handleChallenge(User sender, User receiver, ChallengeHandleType handleType) async{
 
-    String handleTypeString = handleType.toString().split('.').last;
-    String url = 'https://us-central1-tic-tac-toe-a69ee.cloudfunctions.net/handleChallenge?senderId='+senderId+'&senderName='+senderName+'&senderFcmToken='+senderFcmToken+'&receiverId='+receiverId+'&receiverName='+receiverName+'&receiverFcmToken='+receiverFcmToken+'&handleType='+handleTypeString;
-    
-    try{
-      final response =
-        await http.get(url); 
-      if (response.statusCode == 200) {
-        // If server returns an OK response, parse the JSON
-        return true;
 
-      } else {
-        // If that response was not OK, throw an error.
-        throw Exception('server error in challenge');
-      }
-    }catch(err){
-      throw Exception('Could not send challenge');
-    }
-
+    String handleTypeString = handleType.toString().split('.').last; //remove from here...
+    String url = 'https://us-central1-tic-tac-toe-a69ee.cloudfunctions.net/handleChallenge?senderId='+sender.id+'&senderName='+sender.name+'&senderFcmToken='+sender.fcmToken+'&receiverId='+receiver.id+'&receiverName='+receiver.name+'&receiverFcmToken='+receiver.fcmToken+'&handleType='+handleTypeString; 
+    return _sendGetRequest(url);
 
   }
 
   Future<bool> playPiece(String gameId, String playerId, int position) async{
 
     String url = 'https://us-central1-tic-tac-toe-a69ee.cloudfunctions.net/playPiece?gameId='+gameId+'&playerId='+playerId+'&position='+position.toString();
-
-    print(url);
-
-    try{  
-        final response =
-          await http.get(url); 
-
-        if (response.statusCode == 200) {
-          // If server returns an OK response, parse the JSON
-          return true;
-
-        } else {
-          // If that response was not OK, throw an error.
-          throw Exception('error from server');
-        }
-    }catch(err){
-        throw Exception('Could not play piece');
-    }
+    return _sendGetRequest(url);
 }
 
  Future<bool> replayGame(String gameId, String playerId) async{
 
     String url = 'https://us-central1-tic-tac-toe-a69ee.cloudfunctions.net/replayGame?gameId='+gameId+'&playerId='+playerId;
-
-    print(url);
-
-    try{  
-        final response =
-          await http.get(url); 
-
-        if (response.statusCode == 200) {
-          // If server returns an OK response, parse the JSON
-          return true;
-        } else {
-          // If that response was not OK, throw an error.
-          throw Exception('error from server');
-        }
-    }catch(err){
-        throw Exception('error while trying to repeat');
-    }
-
+    return _sendGetRequest(url);
 
 }
 
 Future<bool> cancelGame(String gameId, String playerId) async{
 
     String url = 'https://us-central1-tic-tac-toe-a69ee.cloudfunctions.net/cancelGame?gameId='+gameId+'&playerId='+playerId;
+    return _sendGetRequest(url);
 
-    print(url);
 
-    try{  
+}
+
+
+Future<bool> _sendGetRequest(String url) async{
+
+   try{  
         final response =
           await http.get(url); 
 
         if (response.statusCode == 200) {
-          // If server returns an OK response, parse the JSON
           return true;
         } else {
-          // If that response was not OK, throw an error.
           throw Exception('error from server');
         }
     }catch(err){
         throw Exception('error while trying to repeat');
     }
-
-
 }
-
 
 }
 
